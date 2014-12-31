875c260a4ba9b526576eb279739c44b23e76e1df
zach pratt <zach@attackofzach.com>
2014-12-31 13:59:11 -0600

    Use the shared ampersand cpu load model.

--- index.js
+++ index.js
@@ -5,23 +5,28 @@
         React = require('react'),
 
         CpuDisplay = require('./lib/cpu-display'),
+        CpuLoadModel = require('./lib/cpu-load-model'),
 
         URL = 'http://localhost:8080',
         socket = io(URL);
 
     function init() {
-        var rendered = React.render(
+        var cpuLoadModel = new CpuLoadModel({
+                load: 0,
+                lastUpdateTs: Date.now()
+            }),
+            cpuDisplayComponent = React.render(
             React.createElement(CpuDisplay, {title: "Total User Time"}),
             document.body
         );
 
         socket.on('cpu_update', function (data) {
-            rendered.setState({
-                load: data.totalUserTime
-            });
+            cpuLoadModel.set(data);
+
+            cpuDisplayComponent.setState(cpuLoadModel.toJSON());
         });
 
-        window.rendered = rendered;
+        window.rendered = cpuDisplayComponent;
     }
 
     init();
