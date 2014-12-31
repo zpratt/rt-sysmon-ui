c69c640d404aba92638bc83cd7e026c2c2a70378
zach pratt <zach@attackofzach.com>
2014-12-31 10:41:04 -0600

    Re-render the CpuDisplay component on change of cpu load.

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
     'use strict';
 
     var io = require('socket.io-client'),
+        React = require('react'),
+
+        CpuDisplay = require('./lib/cpu-display'),
 
         URL = 'http://localhost:8080',
-
         socket = io(URL);
 
+    var el = React.createElement(CpuDisplay, {name: "World"});
+
+    var rendered = React.render(el, document.body);
+
     socket.on('cpu_update', function (data) {
-        document.querySelector('body').innerHTML = data.totalUserTime;
+        rendered.setState({
+            load: data.totalUserTime
+        });
     });
+
+    window.rendered = rendered;
 }());
