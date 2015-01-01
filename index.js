5f433ebb9fb11bec2949ca990f094483d4edd7c6
zach pratt <zach@attackofzach.com>
2014-12-31 23:44:32 -0600

    WIP: Visualize the cpu load as a line graph

--- index.js
+++ index.js
@@ -11,19 +11,23 @@
         socket = io(URL);
 
     function init() {
-        var cpuLoadModel = new CpuLoadModel({
+        var loadStack = [],
+            cpuLoadModel = new CpuLoadModel({
                 load: 0,
                 lastUpdateTs: Date.now()
             }),
             cpuDisplayComponent = React.render(
-            React.createElement(CpuDisplay, {title: "Total User Time"}),
-            document.body
-        );
+                React.createElement(CpuDisplay, {title: "Total User Time"}),
+                document.body
+            );
 
         socket.on('cpu_update', function (data) {
             cpuLoadModel.set(data);
+            loadStack.push(cpuLoadModel.toJSON());
 
-            cpuDisplayComponent.setState(cpuLoadModel.toJSON());
+            cpuDisplayComponent.setState({
+                loadData: loadStack
+            });
         });
 
         window.rendered = cpuDisplayComponent;
